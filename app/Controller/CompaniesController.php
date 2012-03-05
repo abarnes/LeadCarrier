b7641932311c472e791bc43652189a3afd5db018
Austin Barnes <austin@austinbarnes.net>
2012-03-04 23:57:52 -0600

    testing vendor login

--- app/Controller/CompaniesController.php
+++ app/Controller/CompaniesController.php
@@ -283,7 +283,7 @@ class CompaniesController extends AppController {
 		    }
 		
 		$v = $this->Vendor->find('first',array('conditions'=>array('Vendor.token'=>$token)));
-		die(print_r($v));
+		die(print_r($company));
 		$this->set('vendor',$v);
 		if (!empty($this->request->data)) {
 			$this->request->data['User']['username'] = $token;
