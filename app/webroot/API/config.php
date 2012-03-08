899777e7d919b6b9df87599b4f58e8b3d8bd106e
Austin Barnes <austin@austinbarnes.net>
2012-03-07 23:03:06 -0600

    updated bill shell

--- app/webroot/API/config.php
+++ app/webroot/API/config.php
@@ -8,8 +8,8 @@ Configure your API here
 $company_name='Barnespos';                                                            //your company name (capitalization matters!)
 $apitoken = 'apitoken';                                                               //your company's API token
 $site_url = 'http://localhost:8888';                                                  //your own website URL (not your lead carrier access URL)        
-$leadcarrier_url = 'http://localhost:8888';                                                  //your company's Lead Carrier access url (e.g. company.leadcarrier.com)
-$submit_type = 'separate';                                                            //If you are submitting client information AND industry selections at once, set this to 'together'
+$leadcarrier_url = 'http://austintest.leadcarrier.com';                                                  //your company's Lead Carrier access url (e.g. company.leadcarrier.com)
+$submit_type = 'together';                                                            //If you are submitting client information AND industry selections at once, set this to 'together'
 // If $submit_type is set to separate:                                                //For separate information and industry selection submissions, set this to 'separate'
     $selection_url = 'http://localhost:8888/selectiontest.html';                      //if using separate pages for information and selections, specify the URL of the selections page
 
