4cac620ee3ea1b336eae0e9517ec9a44a516b50c
Austin Barnes <austin@austinbarnes.net>
2012-03-08 00:17:50 -0600

    testing

--- app/Console/Command/BillShell.php
+++ app/Console/Command/BillShell.php
@@ -54,7 +54,7 @@ class BillShell extends Shell {
 						$this->Record->saveField('bill_id',$bill_id);
 						$this->Record->id = false;
 						
-						$lines[0] += array('line'=>array('name'=>'Lead','unit_cost'=>$s['Setting']['lead_price'],'quantity'=>'1','description'=>date('Y-m-d',strtotime($r['Record']['created'])).$r['Client']['first_name'].' '.$r['Client']['last_name']));
+						$lines += array('line'=>array('name'=>'Lead','unit_cost'=>$s['Setting']['lead_price'],'quantity'=>'1','description'=>date('Y-m-d',strtotime($r['Record']['created'])).$r['Client']['first_name'].' '.$r['Client']['last_name']));
 					}
 					die(print_r($lines));
 					
