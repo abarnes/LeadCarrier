56c0084463325a0da3be0f64582e0f4cd3571342
Austin Barnes <austin@austinbarnes.net>
2012-04-01 16:55:32 -0500

    testing

--- app/Console/Command/LeadShell.php
+++ app/Console/Command/LeadShell.php
@@ -29,14 +29,14 @@ class LeadShell extends Shell {
 				$vendor = $this->Vendor->findById($r['Record']['vendor_id']);
 				$client = $this->Client->findById($r['Record']['client_id']);
 				
-				$email->set('name',$s['Setting']['site_url']);
+				/*$email->set('name',$s['Setting']['site_url']);
 				$email->set('c',$client);
 				if ($range!=null) {
 					$r = $this->Range->findById($range);
 					$email->set('rr','Price Range: '.$r['Range']['name']);
 				} else {
 					$email->set('rr','');
-				} 
+				}*/
 				
 				// Let the vendor know
 				$opts = array();	
