dd0ff836885a96367071ce467a053867af1359bc
Austin Barnes <austin@austinbarnes.net>
2012-04-01 16:05:15 -0500

    testing email

--- app/Console/Command/LeadShell.php
+++ app/Console/Command/LeadShell.php
@@ -1,10 +1,13 @@
 <?php
 class LeadShell extends Shell {
 	var $uses = array('Vendor','Setting','Company','Record','Client');
-	var $tasks = array('Email');
-	var $Email;
+	//var $tasks = array('Email');
+	//var $Email;
 
 	public function main() {
+		App::uses('CakeEmail', 'Network/Email');
+		$email = new CakeEmail();
+		
 		$companies = $this->Company->find('all',array('order'=>'Company.id ASC','conditions'=>array('Company.active'=>'1','Company.id !='=>'1')));
 		foreach ($companies as $c) {
 			$connect = array('db_name'=>$c['Company']['db_name'],'db_password'=>$c['Company']['db_password']);
@@ -28,23 +31,23 @@ class LeadShell extends Shell {
 				$vendor = $this->Vendor->findById($r['Record']['vendor_id']);
 				$client = $this->Client->findById($r['Record']['client_id']);
 				
-				$this->Email->set('name',$s['Setting']['site_url']);
-				$this->Email->set('c',$client);
+				$email->set('name',$s['Setting']['site_url']);
+				$email->set('c',$client);
 				if ($range!=null) {
 					$r = $this->Range->findById($range);
-					$this->Email->set('rr','Price Range: '.$r['Range']['name']);
+					$email->set('rr','Price Range: '.$r['Range']['name']);
 				} else {
-					$this->Email->set('rr','');
+					$email->set('rr','');
 				}
 				
 				// Let the vendor know
-				$this->Email->to = $v['Vendor']['email'];
-				$this->Email->subject = 'Lead from '.$s['Setting']['site_url'];
-				$this->Email->replyTo = $s['Setting']['replyto_email'];
-				$this->Email->from =  $s['Setting']['site_url'].' <'.$s['Setting']['replyto_email'].'>';
-				$this->Email->template = 'lead'; 
-				$this->Email->sendAs = 'both';
-				$this->Email->send();
+				$email->to = $v['Vendor']['email'];
+				$email->subject = 'Lead from '.$s['Setting']['site_url'];
+				$email->replyTo = $s['Setting']['replyto_email'];
+				$email->from =  $s['Setting']['site_url'].' <'.$s['Setting']['replyto_email'].'>';
+				$email->template = 'lead'; 
+				$email->sendAs = 'both';
+				$email->send();
 				
 				$this->Record->id = $r['Record']['id'];
 				$this->Record->saveField('sent','1');
