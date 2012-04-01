26fadc93a0688df2179c6bcc3a4c8305ecac7edf
Austin Barnes <austin@austinbarnes.net>
2012-04-01 17:11:18 -0500

    testing

--- app/Controller/ClientsController.php
+++ app/Controller/ClientsController.php
@@ -353,8 +353,7 @@ class ClientsController extends AppController {
 	function _mail($to,$id,$range=null){
 		$s = $this->Setting->find('first',array('order'=>'Setting.created DESC'));
 		$client = $this->Client->findById($id);
-		$this->set('name',$s['Setting']['site_url']);
-		$this->set('c',$client);
+
 		if ($range!=null) {
 			$r = $this->Range->findById($range);
 			$this->set('rr','Price Range: '.$r['Range']['name']);
